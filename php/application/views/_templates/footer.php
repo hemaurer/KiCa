1cd899fe47962a9e68ab3e851983f6b35f380540
AllTernative90 <->
2014-05-11 18:47:46 +0200

    Erste Anpassung: Header/Footer
    -Header und Footer in ersten Zügen angepasst, dass alle Elemente hier zu
    finden sind. Header ist im ausgeloggten Zustand, für Elemente die nur
    von eingeloggten Usern zu sehen sind: Issue auf Github ansehen.
    -Styling über CSS nur in Grundzügen, da noch kein Design abgesprochen
    -MVC Struktur DOC: kleine Korrekturen

--- php/application/views/_templates/footer.php
+++ php/application/views/_templates/footer.php
@@ -1,2 +1,8 @@
+	<div class="footer">
+		<ul>
+		   <li>Copyright KiCa 2014</li>
+		   <li><a href="#">Impressum</a></li>
+		</ul>
+	</div>
 </body>
 </html>
--- php/application/views/_templates/header.php
+++ php/application/views/_templates/header.php
@@ -9,39 +9,45 @@
     <link href="<?php echo URL; ?>public/css/style.css" rel="stylesheet">
     <!-- jQuery -->
     <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
-    <!-- our JavaScript -->
-    <script src="<?php echo URL; ?>public/js/application.js"></script>
+
 </head>
 <body>
 <!-- header -->
 <div class="container">
-    <!-- Info -->
-    <div class="where-are-we-box">
-        Everything in this box is loaded from <span class="bold">application/views/_templates/header.php</span> !
-        <br />
-        The green line is added via JavaScript (to show how to integrate JavaScript).
-    </div>
-    <h1>The header (used on all pages)</h1>
-    <!-- demo image -->
-    <h3>Demo image, to show usage of public/img folder</h3>
+    <!-- KiCa Logo -->
     <div>
-        <img src="<?php echo URL; ?>public/img/demo-image.png" />
+        <img src="<?php echo URL; ?>public/img/logo.png" />
     </div>
     <!-- navigation -->
-    <h3>Demo Navigation</h3>
-    <div class="navigation">
+    <nav>
         <ul>
             <!-- same like "home" or "home/index" -->
-			<li><a href="<?php echo URL; ?>verwaltung/"><?php echo URL; ?>verwaltung</a></li>
-            <li><a href="<?php echo URL; ?>"><?php echo URL; ?>home</a></li>
-            <li><a href="<?php echo URL; ?>home/exampleone"><?php echo URL; ?>home/exampleone</a></li>
-            <li><a href="<?php echo URL; ?>home/exampletwo"><?php echo URL; ?>home/exampletwo</a></li>
-            <!-- "songs" and "songs/index" are the same -->
-            <li><a href="<?php echo URL; ?>songs/"><?php echo URL; ?>songs/index</a></li>
+            <li><a href="<?php echo URL; ?>">Home</a></li>
+            <li><a href="#">Termine</a></li>
+            <li><a href="#">Ligatabelle</a>
+                <ul>
+                    <li><a href="#">A-Jugend</a></li>
+                    <li><a href="#">B-Jugend</a></li>
+                    <li><a href="#">C-Jugend</a></li>
+                    <li><a href="#">D-Jugend</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Turniere</a></li>
+            <!-- Login mit Submenü, die die Forms beinhalten (und im registrierten Zustand auch weitere Felder) -->
+            <!-- dies muss eventuell über 2 Header oder sonstige Restriktionen geschehen -->
+			<li><a href="#">Login</a>
+                <ul id="login">
+                    <li>
+                        <form name="login" action="<?php echo URL; ?>home/login" method="post">
+                            <input type="text" placeholder="Benutzername" name="username"><br>
+                            <input type="password" placeholder="Passwort" name="password"><br>
+                            <input type="submit" name="Submit">
+                        </form>
+                    </li>
+                    <!-- Verwaltung nur zu Testzwecken hier enthalten - später nur im eingeloggten Zustand an dieser Stelle zu finden -->
+                    <li><a href="<?php echo URL; ?>verwaltung/">Verwaltung</a></li>
+                </ul>
+            </li>
         </ul>
-    </div>
-    <!-- simple div for javascript output, just to show how to integrate js into this MVC construct -->
-    <h3>Demo JavaScript</h3>
-    <div id="javascript-header-demo-box">
-    </div>
+    </nav>
 </div>
