d7c1702c6f8e86d805cf6ba0161369184737c124
pretender16 <->
2014-05-21 21:52:18 +0200

    date und time picker styling anpassung
    aufräumarbeiten, styling der picker an das restliche styling angepasst

--- php/application/views/_templates/header.php
+++ php/application/views/_templates/header.php
@@ -6,10 +6,12 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- css -->
+	
     <link href="<?php echo URL; ?>public/css/style.css" rel="stylesheet">
 	<link href="<?php echo URL; ?>public/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo URL; ?>public/css/datepicker3.css" rel="stylesheet">
 	<link href="<?php echo URL; ?>public/css/bootstrap-timepicker.css" rel="stylesheet">
-	<link href="<?php echo URL; ?>public/css/bootstrap-datepicker3.css" rel="stylesheet">
+	
     <!-- jQuery -->
     <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
 	<script type="text/javascript" src="<?php echo URL; ?>public/js/bootstrap.min.js"></script>
