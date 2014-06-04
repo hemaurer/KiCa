159e9f4ce31239aefc5421a258c6d3771876702c
AllTernative90 <->
2014-06-04 13:29:38 +0200

    Bugfix Kalender
    Komma hat im Quelltext gefehlt (?) - jetzt funktionierts

--- php/application/views/home/index.php
+++ php/application/views/home/index.php
@@ -11,7 +11,7 @@
 
 		 	{events:<?php echo $trainingseinheitenDaten;?>, backgroundColor: '#3C763D', borderColor : '#3C763D', textColor:'#fff'},
 		 	{events:<?php echo $ligaDaten;?>,backgroundColor: '#428BCA', borderColor: '#428BCA'},
-		 	{events:<?php echo $turnierDaten;?>,backgroundColor: '#A94442', borderColor:'#A94442', textColor:'#fff'}
+		 	{events:<?php echo $turnierDaten;?>,backgroundColor: '#A94442', borderColor:'#A94442', textColor:'#fff'},
 		 	{events:<?php echo $freundschaftsDaten;?>,backgroundColor: '#B80591', borderColor:'#B80591', textColor:'#fff'},
 
 			]
