19d8529bc8964dd003a9414cfedc198efede2d9d
Immurb <T.Witthohn92@web.de>
2014-06-12 13:14:25 +0200

    2 neue SQL-Befehle

---
+++ DB/Befehle/Heimmannschaft Verwaltung.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT mannschaft.name AS Mannschaft
+	FROM mannschaft
+		JOIN mannschaft_turnier_sparte ON mannschaft.m_id = mannschaft_turnier_sparte.m_id
+		JOIN turnier ON turnier.tu_id = mannschaft_turnier_sparte.tu_id
+		JOIN sparte ON sparte.sparte_id = mannschaft_turnier_sparte.sparte_id
+	WHERE IFNULL(turnier.name = NULL,TRUE) and IFNULL(sparte.name = NULL,TRUE)
+	ORDER BY Mannschaft;
