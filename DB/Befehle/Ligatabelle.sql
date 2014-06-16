3f1ffa0dbd9c4a6b422952310b1584856a700efe
Immurb <T.Witthohn92@web.de>
2014-06-16 12:11:26 +0200

    Anpassung Ligatabelle
    Es werden jetzt die Ligen der Mannschaft mit ID 1 angezeigt.

--- DB/Befehle/Ligatabelle.sql
+++ DB/Befehle/Ligatabelle.sql
@@ -11,6 +11,10 @@ CONCAT((SELECT IFNULL(SUM(spiel.h_tore),0) FROM spiel WHERE spiel.heim = mannsch
 ((SELECT IFNULL(SUM(spiel.h_tore),0) FROM spiel WHERE spiel.auswaerts = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = 1) + (SELECT IFNULL(SUM(spiel.a_tore),0) FROM spiel WHERE spiel.heim = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = 1)) AS TD
 FROM mannschaft
 JOIN mannschaft_turnier_sparte ON mannschaft.m_id = mannschaft_turnier_sparte.m_id
-WHERE mannschaft_turnier_sparte.tu_id = 1 and mannschaft_turnier_sparte.sparte_id = 1
+WHERE mannschaft_turnier_sparte.tu_id = (SELECT  turnier.tu_id
+															FROM turnier
+															JOIN mannschaft_turnier_sparte ON turnier.tu_id = mannschaft_turnier_sparte.tu_id
+															WHERE turnier.liga= TRUE AND mannschaft_turnier_sparte.m_id=1 AND mannschaft_turnier_sparte.sparte_id = 1)
+		AND mannschaft_turnier_sparte.sparte_id = 1
 ORDER BY Punkte DESC,
 			TD DESC
--- php/application/models/ligamodel.php
+++ php/application/models/ligamodel.php
@@ -19,21 +19,25 @@ class LigaModel
     {
 
         $sql = "SELECT mannschaft.name AS Team, 
-				(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id or spiel.auswaerts = mannschaft.m_id) and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id and spiel.h_tore IS NOT NULL and spiel.a_tore IS NOT NULL) AS Spiele,
-				(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id and spiel.h_tore > spiel.a_tore or  spiel.auswaerts = mannschaft.m_id and spiel.h_tore < spiel.a_tore) and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id) AS S,
-				(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id or spiel.auswaerts = mannschaft.m_id) and spiel.h_tore = spiel.a_tore and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) AS U,
-				(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id and spiel.h_tore < spiel.a_tore or  spiel.auswaerts = mannschaft.m_id and spiel.h_tore > spiel.a_tore) and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) AS N,
-				(SELECT COUNT(*)*3 FROM spiel WHERE (spiel.heim = mannschaft.m_id and spiel.h_tore > spiel.a_tore or  spiel.auswaerts = mannschaft.m_id and spiel.h_tore < spiel.a_tore) and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) + 
+					(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id or spiel.auswaerts = mannschaft.m_id) and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id and spiel.h_tore IS NOT NULL and spiel.a_tore IS NOT NULL) AS Spiele,
+					(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id and spiel.h_tore > spiel.a_tore or  spiel.auswaerts = mannschaft.m_id and spiel.h_tore < spiel.a_tore) and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id) AS S,
+					(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id or spiel.auswaerts = mannschaft.m_id) and spiel.h_tore = spiel.a_tore and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) AS U,
+					(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id and spiel.h_tore < spiel.a_tore or  spiel.auswaerts = mannschaft.m_id and spiel.h_tore > spiel.a_tore) and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) AS N,
+					(SELECT COUNT(*)*3 FROM spiel WHERE (spiel.heim = mannschaft.m_id and spiel.h_tore > spiel.a_tore or  spiel.auswaerts = mannschaft.m_id and spiel.h_tore < spiel.a_tore) and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) + 
 					(SELECT COUNT(*) FROM spiel WHERE (spiel.heim = mannschaft.m_id or spiel.auswaerts = mannschaft.m_id) and spiel.h_tore = spiel.a_tore and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) AS Punkte,
-				CONCAT((SELECT IFNULL(SUM(spiel.h_tore),0) FROM spiel WHERE spiel.heim = mannschaft.m_id and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) +  (SELECT IFNULL(SUM(spiel.a_tore),0) FROM spiel WHERE spiel.auswaerts = mannschaft.m_id and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id),':', 
+					CONCAT((SELECT IFNULL(SUM(spiel.h_tore),0) FROM spiel WHERE spiel.heim = mannschaft.m_id and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id) +  (SELECT IFNULL(SUM(spiel.a_tore),0) FROM spiel WHERE spiel.auswaerts = mannschaft.m_id and spiel.stat_id = 1  and spiel.tu_id = 1 and spiel.sparte_id = :s_id),':', 
 					(SELECT IFNULL(SUM(spiel.h_tore),0) FROM spiel WHERE spiel.auswaerts = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id) +  (SELECT IFNULL(SUM(spiel.a_tore),0) FROM spiel WHERE spiel.heim = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id)) AS Tore,
-				(SELECT IFNULL(SUM(spiel.h_tore),0) FROM spiel WHERE spiel.heim = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id) +  (SELECT IFNULL(SUM(spiel.a_tore),0) FROM spiel WHERE spiel.auswaerts = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id) -
+					(SELECT IFNULL(SUM(spiel.h_tore),0) FROM spiel WHERE spiel.heim = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id) +  (SELECT IFNULL(SUM(spiel.a_tore),0) FROM spiel WHERE spiel.auswaerts = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id) -
 					((SELECT IFNULL(SUM(spiel.h_tore),0) FROM spiel WHERE spiel.auswaerts = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id) + (SELECT IFNULL(SUM(spiel.a_tore),0) FROM spiel WHERE spiel.heim = mannschaft.m_id and spiel.stat_id = 1 and spiel.tu_id = 1 and spiel.sparte_id = :s_id)) AS TD
 				FROM mannschaft
 					JOIN mannschaft_turnier_sparte ON mannschaft.m_id = mannschaft_turnier_sparte.m_id
-				WHERE mannschaft_turnier_sparte.tu_id = 1 and mannschaft_turnier_sparte.sparte_id = :s_id
+				WHERE mannschaft_turnier_sparte.tu_id = (SELECT  turnier.tu_id
+														FROM turnier
+															JOIN mannschaft_turnier_sparte ON turnier.tu_id = mannschaft_turnier_sparte.tu_id
+														WHERE turnier.liga= TRUE AND mannschaft_turnier_sparte.m_id=1 AND mannschaft_turnier_sparte.sparte_id = :s_id)
+					AND mannschaft_turnier_sparte.sparte_id = :s_id
 				ORDER BY Punkte DESC,
-							TD DESC";
+						TD DESC";
         $query = $this->db->prepare($sql);
         $query->execute(array(':s_id' => $s_id));
 
