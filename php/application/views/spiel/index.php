78f48a890ff11c517b86e9eeec164831ee78c074
pretender16 <->
2014-06-25 02:11:31 +0200

    Turnier: Spiel-Seite gestyled
    Footer gefixed, bootstrap styling

--- php/application/views/spiel/index.php
+++ php/application/views/spiel/index.php
@@ -4,81 +4,94 @@
 
 
 <div class="container">
-	<h4>Details</h4>
-	<div>
-	<?php $date = new DateTime($spiel->Uhrzeit); // Datumsformatierer vorbereiten?>
-		<table>
-			<tr>
-				<td>Heim Team</td>
-				<td><?=$spiel->Heim?></td>
-			</tr>
-			<tr>
-				<td>Gegnerisches Team</td>
-				<td><?=$spiel->Auswaerts ?></td>
-			</tr>
-			<?php if (isset($spiel->Heimtore)) { ?>
-				<tr>
-					<td>Ergebnis</td>
-					<td><?=$spiel->Heimtore.":".$spiel->Auswaertstore?></td>
-				</tr>
-			<?php } ?>
-			<tr>
-				<td>Spielart</td>
-				<td><?=$spiel->Turnier?></td>
-			</tr>
-			<tr>
-				<td>Ort</td>
-				<td><?=$spiel->Ort?></td>
-			</tr>
-			<tr>
-				<td>Zeit</td>
-				<td><?=$date->format('H:i')?> Uhr</td>
-			</tr>
-		</table>
-	</div>
+	<div class="form-horizontal">
+		<div class="form-group">
+			<label class="control-label col-md-3">Details</label>
+			<div class="col-md-6">
+			<?php $date = new DateTime($spiel->Uhrzeit); // Datumsformatierer vorbereiten?>
+				<table class="table">
+					<tr>
+						<th>Heim Team</th>
+						<td><?=$spiel->Heim?></td>
+					</tr>
+					<tr>
+						<th>Gegnerisches Team</th>
+						<td><?=$spiel->Auswaerts ?></td>
+					</tr>
+					<?php if (isset($spiel->Heimtore)) { ?>
+						<tr>
+							<th>Ergebnis</th>
+							<td><?=$spiel->Heimtore.":".$spiel->Auswaertstore?></td>
+						</tr>
+					<?php } ?>
+					<tr>
+						<th>Spielart</th>
+						<td><?=$spiel->Turnier?></td>
+					</tr>
+					<tr>
+						<th>Ort</th>
+						<td><?=$spiel->Ort?></td>
+					</tr>
+					<tr>
+						<th>Zeit</th>
+						<td><?=$date->format('H:i')?> Uhr</td>
+					</tr>
+				</table>
+			</div>
+		</div>
 
-	<h4>Aufstellung</h4>
-	<div class="checkbox">
-		<form class="form-horizontal" action="<?php echo URL; ?>spiel/edit_aufstellung/" method="POST">
-			<input type="hidden" name="s_id" value="<?php echo $s_id ?>">
-			<table>
-			<?php 
-				// Liste alle Personen auf
-				foreach ($personen as $spieler){
-					$b_aufgestellt = false;
-					echo "";
-					// Betreuer auslassen
-					if ($spieler->betreuer == 0) {
-						// Wenn keine Aufstellung bisher vorhanden ist, muss nicht geprüft werden, ob er aufgestellt ist
-						if (count($aufstellung) > 0){
-							foreach ($aufstellung as $aufgestellterSpieler) {
-								if (strstr($aufgestellterSpieler->Spieler, $spieler->name) !== false){
-									$b_aufgestellt = true;
-									break;
-								} 
-							}
-							// Checkbox darstellen, checked oder nicht
-							if ($b_aufgestellt){
-								echo "<tr><td><input type='checkbox' name='arr_aufstellung[]' value='".$spieler->p_id."' checked /> </td><td>";
-							} else {
-								echo "<tr><td><input type='checkbox' name='arr_aufstellung[]' value='".$spieler->p_id."' /> </td><td>";
-							}
-							
-						} else {
-							echo "<tr><td><input type='checkbox' name='arr_aufstellung[]' value='".$spieler->p_id."' />";
-							
-						}
-						echo $spieler->v_name." ".$spieler->name."</td></tr>";
-						
-					}
-				}
-				
-			?>
-			</table>
-			<br />
-			<input class="btn btn-default" type="submit" name="submit_edit_aufstellung" value="Speichern" />
-		</form>
-	<div class="checkbox">
+
+		<div class="form-group">
+			<form action="<?php echo URL; ?>spiel/edit_aufstellung/" method="POST">
+				<input type="hidden" name="s_id" value="<?php echo $s_id ?>">
+				<div class="form-group">
+					<label class="control-label col-md-3">Aufstellung</label>
+					<div class="col-md-6">
+						<ul class="form-control" id="checkboxSelect">
+							<div class="checkbox">
+							<?php 
+								// Liste alle Personen auf
+								foreach ($personen as $spieler){
+									$b_aufgestellt = false;
+									echo "";
+									// Betreuer auslassen
+									if ($spieler->betreuer == 0) {
+										// Wenn keine Aufstellung bisher vorhanden ist, muss nicht geprüft werden, ob er aufgestellt ist
+										if (count($aufstellung) > 0){
+											foreach ($aufstellung as $aufgestellterSpieler) {
+												if (strstr($aufgestellterSpieler->Spieler, $spieler->name) !== false){
+													$b_aufgestellt = true;
+													break;
+												} 
+											}
+											// Checkbox darstellen, checked oder nicht
+											if ($b_aufgestellt){
+												echo "<li><label><input type='checkbox' name='arr_aufstellung[]' value='".$spieler->p_id."' checked /></label></li>";
+											} else {
+												echo "<li><label><input type='checkbox' name='arr_aufstellung[]' value='".$spieler->p_id."' /> </label></li>";
+											}
+											
+										} else {
+											echo "<li><label><input type='checkbox' name='arr_aufstellung[]' value='".$spieler->p_id."' />";
+											
+										}
+										echo $spieler->v_name." ".$spieler->name."</label></li>";
+									}
+								}
+							?>
+							</div>
+						</ul>
+					</div>
+				</div>
+				<br />
+				<div class="form-group">
+					<div class="col-md-offset-3 col-md-6">
+						<input class="btn btn-default" type="submit" name="submit_edit_aufstellung" value="Speichern" />
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
 </div>
 
  <?php
