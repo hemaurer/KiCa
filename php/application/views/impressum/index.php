1b4a8620b0f2d1934ffe209529767008fbe12af3
AllTernative90 <->
2014-07-02 16:45:27 +0200

    Home + Impressum: Tabellen Design
    Tabellen Design (Spaltenbreite und Ausrichtung) der Kalender onclick
    Tabellen und des Impressums angepasst.

--- php/application/views/impressum/index.php
+++ php/application/views/impressum/index.php
@@ -9,14 +9,14 @@
 			<table class="table" >
 				<thead style="background-color: #ddd; font-weight: bold;">
 					<tr>
-						<td colspan="3">
+						<td colspan="2" >
 							<span class="glyphicon glyphicon-home"></span> Firmeninformationen
 						</td>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td><strong>Kickers Calendar</strong></td>
+						<td width="50%"><strong>Kickers Calendar</strong></td>
 						<td><span class="glyphicon glyphicon-earphone"></span> 0650 13371234</td>
 					</tr>
 					<tr>
@@ -41,17 +41,17 @@
 			<table class="table" >
 				<thead style="background-color: #ddd; font-weight: bold;">
 					<tr>
-						<td colspan="3">
+						<td colspan="2">
 							<span class="glyphicon glyphicon-user"></span> Rechtliche Angaben
 						</td>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td><strong>Inhaltlich Verantwortlicher gemäß TMG:</strong></td>
+						<td colspan="2"><strong>Inhaltlich Verantwortlicher gemäß TMG:</strong></td>
 					</tr>
 					<tr>
-						<td>KiCa-Team</td>
+						<td width="50%">KiCa-Team</td>
 						<td><span class="glyphicon glyphicon-envelope"></span> <a href="#">info@KiCa.tld</a></td>
 					</tr>
 				</tbody>
@@ -61,14 +61,14 @@
 			<table class="table" >
 				<thead style="background-color: #ddd; font-weight: bold;">
 					<tr>
-						<td colspan="3">
+						<td colspan="2">
 							<span class="glyphicon glyphicon-hdd"></span> Technische Angaben
 						</td>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td><strong>Technisch Verantwortlicher:</strong></td>
+						<td width="50%"><strong>Technisch Verantwortlicher:</strong></td>
 						<td><a href="#">1337-Design GbR Leipzig</a></td>
 					</tr>
 				</tbody>
@@ -78,14 +78,14 @@
 			<table class="table" >
 				<thead style="background-color: #ddd; font-weight: bold;">
 					<tr>
-						<td colspan="3">
+						<td colspan="2">
 							<span class="glyphicon glyphicon-camera"></span> Bildquellen und -rechte
 						</td>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td><a href="http://www.roth-foto.de">Roth Foto</a></td>
+						<td width="50%"><a href="http://www.roth-foto.de">Roth Foto</a></td>
 						<td><a href="http://www.querfeldein-fotos.de">Querfeldein Fotos</a></td>
 					</tr>
 				</tbody>
@@ -95,14 +95,14 @@
 			<table class="table" >
 				<thead style="background-color: #ddd; font-weight: bold;">
 					<tr>
-						<td colspan="3">
+						<td colspan="2">
 							<span class="glyphicon glyphicon-plus"></span> Weitere Informationen
 						</td>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td><a href="http://www.ep-personaltraining.de/index.php?option=com_impressum&Itemid=55&view=nutzungsbedingungen">Allg. Nutzungsbedingungen anzeigen</a></td>
+						<td width="50%"><a href="http://www.ep-personaltraining.de/index.php?option=com_impressum&Itemid=55&view=nutzungsbedingungen">Allg. Nutzungsbedingungen anzeigen</a></td>
 						<td><a href="http://www.ep-personaltraining.de/index.php?option=com_impressum&Itemid=55&view=datenschutz">Erklärung zum Datenschutz anzeigen</a></td>
 					</tr>
 				</tbody>
--- php/public/js/home.js
+++ php/public/js/home.js
@@ -30,25 +30,26 @@ function kalenderModal(str_className, id, URL, b_isBetreuer){
 			 	var t = returnedData.Uhrzeit.split(/[- :]/);
 			 	returnedData.Uhrzeit = "" + t[2] + "." + t[1] + "." + t[0] + " - " + t[3] + ":" + t[4] + " Uhr";
 
+				//Wenn ein Betreuer eingeloggt ist, einen Bearbeiten-Button zum Modal hinzufügen
+				//Variablen für die Bearbeitenbuttons definieren
+				var str_bearbeiten_button_training = "";
+				var str_bearbeiten_button_spiel = "";
+
+				// Buttons aktivieren
+				if(b_isBetreuer == 1){
+					str_bearbeiten_button_training = '<a href="'+URL+'training/index/'+id+'/"><span class="glyphicon glyphicon-pencil"></span></a>';
+					str_bearbeiten_button_spiel = '<a href="'+URL+'spiel/index/'+id+'/"><span class="glyphicon glyphicon-pencil"></span></a>';
+				}
+
 			 	//Modal zur Anzeige der Details mit den Werten aus dem Post Request befüllen
 				if (str_className == "training"){
-					//Wenn ein Betreuer eingeloggt ist, einen Bearbeiten-Button zum Modal hinzufügen
-					if(b_isBetreuer == 1){
-						$('#kalendermodalbody').html('<table class="table"> <thead style="background-color: #ddd; font-weight: bold;"> <tr> <td>'+returnedData.Name+'</td> <td><a href="'+URL+'training/index/'+id+'/"><span class="glyphicon glyphicon-pencil"></span></a></td> </tr> </thead> <tbody><tr> <td><strong>Ort: </strong></td> <td>'+returnedData.Ort+'</td> </tr> <tr> <td><strong>Uhrzeit: </strong></td> <td>'+returnedData.Uhrzeit+'</td> </tr> <tr> <td><strong>Trainer: </strong></td> <td>'+returnedData.Trainer+'</td> </tr> <tr> <td><strong>Trainingsgruppe: </strong></td> <td>'+returnedData.Trainingsgruppe+'</td> </tr> </tbody> </table>');
-					}else{
-						$('#kalendermodalbody').html('<table class="table"> <thead style="background-color: #ddd; font-weight: bold;"> <tr> <td>'+returnedData.Name+'</td> <td></td> </tr> </thead> <tbody> <tr> <td><strong>Ort: </strong></td> <td>'+returnedData.Ort+'</td> </tr> <tr> <td><strong>Uhrzeit: </strong></td> <td>'+returnedData.Uhrzeit+'</td> </tr> <tr> <td><strong>Trainer: </strong></td> <td>'+returnedData.Trainer+'</td> </tr> <tr> <td><strong>Trainingsgruppe: </strong></td> <td>'+returnedData.Trainingsgruppe+'</td> </tr> </tbody> </table>');
-					}
-						$('#kalendermodalheader').html('Details zur Trainingseinheit');
+					$('#kalendermodalbody').html('<table class="table"> <thead style="background-color: #ddd; font-weight: bold;"> <tr> <td>'+returnedData.Name+'</td> <td align="right">'+str_bearbeiten_button_training+'</td> </tr> </thead> <tbody> <tr> <td><strong>Ort: </strong></td> <td>'+returnedData.Ort+'</td> </tr> <tr> <td><strong>Uhrzeit: </strong></td> <td>'+returnedData.Uhrzeit+'</td> </tr> <tr> <td><strong>Trainer: </strong></td> <td>'+returnedData.Trainer+'</td> </tr> <tr> <td><strong>Trainingsgruppe: </strong></td> <td>'+returnedData.Trainingsgruppe+'</td> </tr> </tbody> </table>');
+					$('#kalendermodalheader').html('Details zur Trainingseinheit');
 				}
 			 	//wenn es sich nicht um eine Trainingseinheit handelt, ist es ein Spiel
 				else{
-					if(b_isBetreuer == 1){
-					//Wenn ein Betreuer eingeloggt ist, einen Bearbeiten-Button zum Modal hinzufügen
-						$('#kalendermodalbody').html('<table class="table"> <thead style="background-color: #ddd; font-weight: bold;"> <tr> <td>'+returnedData.Turnier+'</td> <td></td> <td><a href="'+URL+'spiel/index/'+id+'/"><span class="glyphicon glyphicon-pencil"></span></a></td> </tr> </thead> <tbody> <tr> <td>'+returnedData.Heim+'</td> <td><strong>'+Tore+'</strong></td> <td>'+returnedData.Auswaerts+'</td> </tr> <tr> <td>'+returnedData.Uhrzeit+'</td> <td></td> <td>'+returnedData.Ort+'</td> </tr> </tbody> </table>');
-					}else{
-						$('#kalendermodalbody').html('<table class="table"> <thead style="background-color: #ddd; font-weight: bold;"> <tr> <td>'+returnedData.Turnier+'</td> <td></td> <td></td> </tr> </thead> <tbody> <tr> <td>'+returnedData.Heim+'</td> <td><strong>'+Tore+'</strong></td> <td>'+returnedData.Auswaerts+'</td> </tr> <tr> <td>'+returnedData.Uhrzeit+'</td> <td></td> <td>'+returnedData.Ort+'</td> </tr> </tbody> </table>');
-					}
-						$('#kalendermodalheader').html('Details zum '+str_prefix+'spiel');
+					$('#kalendermodalbody').html('<table class="table"> <thead style="background-color: #ddd; font-weight: bold;"> <tr> <td> '+returnedData.Turnier+'</td> <td colspan="2" align="right"> '+str_bearbeiten_button_spiel+'</td> </tr> </thead> <tbody> <tr> <td width="45%"> '+returnedData.Heim+'</td> <td width="10%" align="center"> <strong>'+Tore+'</strong> </td> <td width="45%" align="right"> '+returnedData.Auswaerts+'</td> </tr> <tr> <td> '+returnedData.Uhrzeit+'</td> <td></td> <td align="right"> '+returnedData.Ort+'</td> </tr> </tbody> </table>');
+					$('#kalendermodalheader').html('Details zum '+str_prefix+'spiel');
 				}
 
 				//Modal anzeigen
